997be60847637c7d70051314bc698febb87e1529
vikaschahar80 <147334626+vikaschahar80@users.noreply.github.com>
2024-06-29 16:01:18 +0530

    permutations

---
+++ 10.cpp
@@ -0,0 +1,22 @@
+class Solution {
+private:
+    void solve(vector<int> nums,vector<vector<int>>& ans,int i){
+        if(i>=nums.size()){
+            ans.push_back(nums);
+            return;
+        }
+        for(int j=i;j<nums.size();j++){
+            swap(nums[i],nums[j]);
+            solve(nums,ans,i+1);
+
+            swap(nums[i],nums[j]);
+        }
+    }
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> ans;
+        int i = 0;
+        solve(nums,ans,i);
+        return ans;
+    }
+};
